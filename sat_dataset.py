4fc47597847c9a0668f8b8878d5a96db49eff5e3
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2023-06-24 12:05:12 +0200

    Sat dataset pytorch class implemented

--- sat_dataset.py
+++ sat_dataset.py
@@ -38,7 +38,7 @@ class SatDataset(Dataset):
     def fill_metadata_dict(self):
         for image_path in self.image_paths:
             img_info = self.extract_info_from_filename(image_path)
-            self.metadata_dict[img_info] = image_path
+            self.metadata_dict[image_path] = img_info
 
     def get_entry_paths(self, path):
         entry_paths = []
@@ -56,14 +56,14 @@ class SatDataset(Dataset):
 
     def __getitem__(self, idx):
         image_path = self.image_paths[idx]
+        metadata = self.metadata_dict[image_path]
         image = Image.open(image_path)
 
-        img_info = self.extract_info_from_filename(image_path)
         image = np.array(image, dtype=np.float32)
         image = image / 255.0
         image = torch.from_numpy(image)
 
-        return image, img_info
+        return image, (metadata.x, metadata.y, metadata.z)  # return as integers
 
     def extract_info_from_filename(self, filename):
         # 16_35582_23023.jpg -> z, x, y
@@ -72,22 +72,98 @@ class SatDataset(Dataset):
         fn = fn.split("_")
         try:
             z, x, y = int(fn[0]), int(fn[1]), int(fn[2])
-        except:
-            print("Error extracting info from the filename in sat images: ", filename)
+            if z < 1:
+                raise ValueError
+        except ValueError:
+            print(
+                "Invalid zoom level or error extracting info from the filename in sat images: ",
+                filename,
+            )
             return None
 
-        return mercantile.Tile(x, y, z)
+        try:
+            tile = mercantile.Tile(x, y, z)
+        except ValueError:
+            print(
+                "Invalid tile or error extracting info from the filename in sat images: ",
+                filename,
+            )
+            return None
+
+        return tile
 
         ### -------------------------- ###
         ### MAP MANIPULATION FUNCTIONS ###
         ### -------------------------- ###
 
     def find_tile(self, lat, lng):
-        for tile in self.metadata_dict.keys():
+        for path, tile in self.metadata_dict.items():
             if self.is_coord_in_a_tile(lat, lng, tile):
-                fpath = self.metadata_dict[tile]
-                ix = self.image_paths.index(fpath)
+                ix = self.image_paths.index(path)
                 return self.__getitem__(ix)
+
+    def is_coord_in_a_tile(self, lat, lng, tile):
+        bbox = mercantile.bounds(tile)
+        min_lng, min_lat, max_lng, max_lat = bbox
+
+        return lat >= min_lat and lat <= max_lat and lng >= min_lng and lng <= max_lng
+
+    ### -------------------------- ###
+    ### DOWNLOADING MAPS FUNCTIONS ###
+    ### -------------------------- ###
+
+    def download_maps(self):
+        headers = {
+            "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection": "keep-alive",
+            "Referer": "https://www.openstreetmap.org/",
+            "Sec-Fetch-Dest": "image",
+            "Sec-Fetch-Mode": "no-cors",
+            "Sec-Fetch-Site": "cross-site",
+            "Sec-GPC": "1",
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+            "sec-ch-ua": '"Not.A/Brand";v="8", "Chromium";v="114", "Brave";v="114"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": '"Linux"',
+        }
+
+        params = {
+            "access_token": "pk.eyJ1Ijoib3BlbnN0cmVldG1hcCIsImEiOiJjbGRlaGp1b3gwOGRtM250NW9sOHhuMmRjIn0.Y3mM21ciEP5Zo5amLJUugg",
+        }
+
+        os.makedirs(f"{self.root_dir}/tiles", exist_ok=True)
+
+        for r_name, bbox in self.bboxes.items():
+            print("Downloading maps for region: ", r_name)
+            for tile in tqdm(
+                mercantile.tiles(bbox[0], bbox[1], bbox[2], bbox[3], self.zoom_level)
+            ):
+                box = mercantile.bounds(tile)
+
+                center_lat = (box.north + box.south) / 2
+                center_lng = (box.east + box.west) / 2
+
+                os.makedirs(f"{self.root_dir}/tiles/{r_name}", exist_ok=True)
+                file_path = f"{self.root_dir}/tiles/{r_name}/{self.zoom_level}_{tile.x}_{tile.y}.jpg"
+
+                if not os.path.exists(file_path):
+                    response = requests.get(
+                        f"https://c.tiles.mapbox.com/v4/mapbox.satellite/{self.zoom_level}/{tile.x}/{tile.y}@2x.jpg",
+                        params=params,
+                        headers=headers,
+                    )
+
+                    if response.status_code == 200:
+                        with open(file_path, "wb") as f:
+                            f.write(response.content)
+                    else:
+                        print(f"Error downloading {file_path}")
+
+
+class MapUtils:
+    def __init__(self):
+        pass
 
     def pixel_to_coord(self, x, y, tile, image_width, image_height):
         bbox = mercantile.bounds(tile)
@@ -117,59 +193,43 @@ class SatDataset(Dataset):
 
         return lat >= min_lat and lat <= max_lat and lng >= min_lng and lng <= max_lng
 
-        ### -------------------------- ###
-        ### DOWNLOADING MAPS FUNCTIONS ###
-        ### -------------------------- ###
+    def find_and_plot(self, lat, lng, tile, map_image):
+        x, y = self.coord_to_pixel(
+            lat, lng, tile, map_image.shape[1], map_image.shape[0]
+        )
+        x, y = int(x), int(y)
 
-    def download_maps(self):
-        headers = {
-            "Accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Connection": "keep-alive",
-            "Referer": "https://www.openstreetmap.org/",
-            "Sec-Fetch-Dest": "image",
-            "Sec-Fetch-Mode": "no-cors",
-            "Sec-Fetch-Site": "cross-site",
-            "Sec-GPC": "1",
-            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
-            "sec-ch-ua": '"Not.A/Brand";v="8", "Chromium";v="114", "Brave";v="114"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Linux"',
-        }
-
-        params = {
-            "access_token": "pk.eyJ1Ijoib3BlbnN0cmVldG1hcCIsImEiOiJjbGRlaGp1b3gwOGRtM250NW9sOHhuMmRjIn0.Y3mM21ciEP5Zo5amLJUugg",
-        }
-
-        os.makedirs(f"{self.root_dir}/tiles", exist_ok=True)
-
-        for r_name, bbox in self.bboxes.items():
-            print("Downloading maps for region: ", r_name)
-            for tile in tqdm(bbox[0], bbox[1], bbox[2], bbox[3], self.zoom_level):
-                box = mercantile.bounds(tile)
-
-                center_lat = (box.north + box.south) / 2
-                center_lng = (box.east + box.west) / 2
-
-                os.makedirs(f"{self.root_dir}/tiles/{r_name}", exist_ok=True)
-                file_path = f"{self.root_dir}/tiles/{r_name}/{self.zoom_level}_{tile.x}_{tile.y}.jpg"
-
-                if not os.path.exists(file_path):
-                    response = requests.get(
-                        f"https://c.tiles.mapbox.com/v4/mapbox.satellite/{self.zoom_level}/{tile.x}/{tile.y}@2x.jpg",
-                        params=params,
-                        headers=headers,
-                    )
-
-                    if response.status_code == 200:
-                        with open(file_path, "wb") as f:
-                            f.write(response.content)
-                    else:
-                        print(f"Error downloading {file_path}")
+        fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+        ax.imshow(map_image)
+        ax.scatter(x, y, color="red")
+        plt.show()
 
 
 def main():
-    pass
+    dataloader = torch.utils.data.DataLoader(
+        SatDataset(root_dir="./sat/"), batch_size=10, shuffle=True
+    )
+    map_utils = MapUtils()
+
+    # for batch_idx, (images, infos) in enumerate(dataloader):
+    #    fig, axs = plt.subplots(2, 5, figsize=(15, 6))
+    #    axs = axs.ravel()
+
+    #    print(infos)
+
+    #    for i in range(len(images)):
+    #        image = images[i]
+    #        info = infos[0][i]
+    #        axs[i].imshow(image)
+    #        print(f"Info for image {i}: {info}")
+    #        axs[i].axis("off")
+
+    #    plt.show()
+
+    img, metadata = dataloader.dataset.find_tile(46.051450, 14.506099)
+    tile = mercantile.Tile(x=metadata[0], y=metadata[1], z=metadata[2])
+
+    map_utils.find_and_plot(46.051450, 14.506099, tile, img)
 
 
 if __name__ == "__main__":
