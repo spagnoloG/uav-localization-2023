e1781dbf4a102e9b741f6c472798696542af620c
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2023-08-25 21:24:02 +0200

    Translate the plot to slovene

--- utils/plot_different_hann_kernels.py
+++ utils/plot_different_hann_kernels.py
@@ -36,8 +36,8 @@ def main():
     ax1.plot(
         hann_kernels[rds_val_best_idx], rds_val[rds_val_best_idx], "c*", markersize=12
     )
-    ax1.set_xlabel("Hann Kernels", fontsize=14)
-    ax1.set_ylabel("RDS Values", fontsize=14, color="b")
+    ax1.set_xlabel("Velikost jedra", fontsize=14)
+    ax1.set_ylabel("RDS", fontsize=14, color="b")
     ax1.tick_params(axis="y", labelcolor="b")
     ax1.legend(loc="upper left")
     ax1.grid(True)
@@ -58,16 +58,17 @@ def main():
         "m*",
         markersize=12,
     )
-    ax2.set_ylabel("Hann Values", fontsize=14, color="r")
+    ax2.set_ylabel("Hanningova izguba", fontsize=14, color="r")
     ax2.tick_params(axis="y", labelcolor="r")
     ax2.legend(loc="upper right")
 
-    # Adjust the layout, making space for the title
-    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    fig.suptitle(
+        "Primerjava rezultatov ob uporabi razlicne velikosti Hanningovega okna",
+        fontsize=12,
+    )
 
-    fig.suptitle("RDS and Hann Results by Kernel Size", fontsize=16, y=1.02)
-
-    plt.show()
+    # Save the figure
+    plt.savefig("./res/plot_different_hann_kernels.png", dpi=200)
 
 
 if __name__ == "__main__":
