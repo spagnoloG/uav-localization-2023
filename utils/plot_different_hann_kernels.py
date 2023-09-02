41a512b16488de490482579aea555df7adddc09b
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2023-09-02 10:53:56 +0200

    Make plot of hanning kernels aligned with x-axis

--- utils/plot_different_hann_kernels.py
+++ utils/plot_different_hann_kernels.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def main():
@@ -22,44 +23,66 @@ def main():
     hann_train_best_idx = np.argmin(hann_train)
     hann_val_best_idx = np.argmin(hann_val)
 
+    # Set Seaborn style
+    sns.set_style("whitegrid")
+
+    # Use Seaborn's color palette
+    colors = sns.color_palette("deep")
+
     fig, ax1 = plt.subplots(figsize=(10, 6))
 
-    # Plot RDS data on primary y-axis
-    ax1.plot(hann_kernels, rds_train, "-o", label="RDS Train", color="b")
-    ax1.plot(hann_kernels, rds_val, "-s", label="RDS Val", color="c")
+    # Plot RDS data on primary y-axis using Seaborn colors
+    ax1.plot(hann_kernels, rds_train, "-o", label="RDS Train", color=colors[0])
+    ax1.plot(hann_kernels, rds_val, "-s", label="RDS Val", color=colors[1])
     ax1.plot(
         hann_kernels[rds_train_best_idx],
         rds_train[rds_train_best_idx],
-        "b*",
+        "*",
+        color=colors[0],
         markersize=12,
     )
     ax1.plot(
-        hann_kernels[rds_val_best_idx], rds_val[rds_val_best_idx], "c*", markersize=12
+        hann_kernels[rds_val_best_idx],
+        rds_val[rds_val_best_idx],
+        "*",
+        color=colors[1],
+        markersize=12,
     )
     ax1.set_xlabel("Velikost jedra", fontsize=14)
-    ax1.set_ylabel("RDS", fontsize=14, color="b")
-    ax1.tick_params(axis="y", labelcolor="b")
+    ax1.set_ylabel("RDS", fontsize=14, color=colors[0])
+    ax1.tick_params(axis="y", labelcolor=colors[0])
     ax1.legend(loc="upper left")
     ax1.grid(True)
 
-    # Create secondary y-axis for Hann values
+    # Adjust x-axis to show odd numbers
+    ax1.set_xticks(
+        [
+            x
+            for x in range(int(min(hann_kernels)), int(max(hann_kernels)) + 1)
+            if x % 2 != 0
+        ]
+    )
+
+    # Create secondary y-axis for Hann values using Seaborn colors
     ax2 = ax1.twinx()
-    ax2.plot(hann_kernels, hann_train, "-o", label="Hann Train", color="r")
-    ax2.plot(hann_kernels, hann_val, "-s", label="Hann Val", color="m")
+    ax2.plot(hann_kernels, hann_train, "-o", label="Hann Train", color=colors[2])
+    ax2.plot(hann_kernels, hann_val, "-s", label="Hann Val", color=colors[3])
     ax2.plot(
         hann_kernels[hann_train_best_idx],
         hann_train[hann_train_best_idx],
-        "r*",
+        "*",
+        color=colors[2],
         markersize=12,
     )
     ax2.plot(
         hann_kernels[hann_val_best_idx],
         hann_val[hann_val_best_idx],
-        "m*",
+        "*",
+        color=colors[3],
         markersize=12,
     )
-    ax2.set_ylabel("Hanningova izguba", fontsize=14, color="r")
-    ax2.tick_params(axis="y", labelcolor="r")
+    ax2.set_ylabel("Hanningova izguba", fontsize=14, color=colors[2])
+    ax2.tick_params(axis="y", labelcolor=colors[2])
     ax2.legend(loc="upper right")
 
     fig.suptitle(
@@ -67,8 +90,7 @@ def main():
         fontsize=12,
     )
 
-    # Save the figure
-    plt.savefig("./res/plot_different_hann_kernels.png", dpi=200)
+    plt.savefig("./res/plot_different_hann_kernels.png", dpi=220)
 
 
 if __name__ == "__main__":
