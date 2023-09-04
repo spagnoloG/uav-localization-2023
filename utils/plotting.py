5e56298e821d6750f747fccc88ec7b53973d0c89
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2023-09-04 12:47:55 +0200

    Update model scheme and update gauss and hanning plots

--- utils/plotting.py
+++ utils/plotting.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import norm
-
+from mpl_toolkits.mplot3d import Axes3D
 
 def hanning_2d(M):
     """
@@ -22,14 +22,22 @@ def gaussian_2d(size, sigma=1):
     kernel = np.exp(-(xx**2 + yy**2) / (2.0 * sigma**2))
     return kernel / np.sum(kernel)
 
-
-def plot_kernel(kernel, title):
-    plt.imshow(kernel, cmap="jet", interpolation="nearest")
-    plt.title(title)
-    plt.colorbar()
-    plt.savefig(title + ".jpg", format="jpg")
+def plot_kernel_3d(kernel, title):
+    fig = plt.figure(figsize=(8, 6))
+    ax = fig.add_subplot(111, projection='3d')
+    
+    x = np.linspace(0, kernel.shape[1]-1, kernel.shape[1])
+    y = np.linspace(0, kernel.shape[0]-1, kernel.shape[0])
+    X, Y = np.meshgrid(x, y)
+    
+    surf = ax.plot_surface(X, Y, kernel, cmap='jet', linewidth=0, antialiased=True)
+    
+    fig.colorbar(surf, ax=ax, shrink=0.6, aspect=10)
+    
+    ax.set_title(title)
+    plt.savefig("./res/" + title + ".png", format="png")
     plt.show()
-
+    plt.close()
 
 # Define size and sigma
 size = 33
@@ -37,8 +45,8 @@ sigma = 10
 
 # Generate and plot 2D Hanning kernel
 hanning_kernel = hanning_2d(size)
-plot_kernel(hanning_kernel, "2D Hanning kernel")
+plot_kernel_3d(hanning_kernel, "Normalizirano Hanningovo jedro")
 
 # Generate and plot 2D Gaussian kernel
 gaussian_kernel = gaussian_2d(size, sigma)
-plot_kernel(gaussian_kernel, "2D Gaussian kernel")
+plot_kernel_3d(gaussian_kernel, "Normalizirano Gaussovo jedro")
